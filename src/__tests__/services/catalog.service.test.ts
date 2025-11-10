df238dccc6d0dab7bc386939e39c16de04b3d298
Renato Di Giacomo <renato_di_giacomo@hotmail.com>
2025-11-10 20:57:05 -0300

    Fix catalog service tests and implement catalog listing functionality

--- src/__tests__/services/catalog.service.test.ts
+++ src/__tests__/services/catalog.service.test.ts
@@ -7,17 +7,38 @@ describe('Catalog Service', () => {
         describe('when catalog is empty ', () => {
             it('should Return an empty array', async () => {
                 //Arrange
-                const expectCatalog: Array<Product> = [];
+                const expectedCatalog: Array<Product> = [];
                 const catalogService = new CatalogService();
 
                 //Act
                 const catalog = catalogService.list();
 
                 //Assert
-                expect(catalog).toEqual(expectCatalog);
+                expect(catalog).toEqual(expectedCatalog);
                 expect(catalog).toHaveLength(0);
             })
-        }
-        )
+        });
+
+        describe('when catalog has products', () => {
+            //Arrange
+            const expectedP1 = new Product('produto1', 10.0);
+            const expectedP2 = new Product('produto2', 20.0);
+
+
+            const expectedCatalog = [expectedP1, expectedP2];
+
+            // Act
+            const catalogService = new CatalogService();
+
+            catalogService.add(expectedP1);
+            catalogService.add(expectedP2);
+            
+            const catalog = catalogService.list();
+
+            // Assert 
+            expect(catalog).toEqual(expectedCatalog);
+            expect(catalog).toHaveLength(2);
+
+        })
     })
 });
--- src/services/catalog.service.ts
+++ src/services/catalog.service.ts
@@ -1,9 +1,16 @@
 import { Product } from "../models/product";
 
 export class CatalogService {
+    private catalog: Array<Product> = [];
     constructor() { }
 
+
+    add(product: Product): void {
+        this.catalog.push(product);
+    }
+
+
     list(): Array<Product> {
-        return [];
+        return this.catalog;
     }
 }
