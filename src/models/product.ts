d6fe82b608bb9e0b3950de1e79762cc767c2a1d6
Renato Di Giacomo <renato_di_giacomo@hotmail.com>
2025-11-10 22:41:25 -0300

    Add tax service and tests for tax calculation based on product category

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,5 +1,10 @@
 export class Product {
     constructor(public name: string, public price: number) {
+    }
+}
 
+export class ProductWithCategory extends Product {
+    constructor(name: string, price: number, public category: "regular" | "premium") {
+        super(name, price);
     }
 }
--- src/services/catalog.service.ts
+++ src/services/catalog.service.ts
@@ -4,18 +4,15 @@ import { ProductRepository } from "../repositories/product.repo";
 export class CatalogService {
     private repo: ProductRepository;
 
-    constructor( repo?: ProductRepository) {
-        this.repo = repo || new ProductRepository();
+    constructor(repo?: ProductRepository) {
+        this.repo = repo ?? new ProductRepository();
     }
-
 
     add(product: Product): void {
         this.repo.populate([...this.repo.all(), product]);
     }
 
-
     list(): Array<Product> {
         return this.repo.all();
     }
-
 }
